0ec7eb816ac96eafe14ac4eb6e3ec45f7f940eba
josepedropinto-pt <josemcp@ua.pt>
2021-10-27 22:36:27 +0100

    ex5 completed

--- Aula2/Ex5/main.py
+++ Aula2/Ex5/main.py
@@ -12,35 +12,68 @@ def printAllCharsUpTo (stop_char):
 def readAllUpTo (stop_char):
 
 
-    counted_numeric_key = 0
-    counted_non_numeric_key=0
+    #Criar a lista e colocar lá material
+
+    keys = [] #começar com lista vazia
 
     while True :
         print('type something (to stop:press "X") ')
         key = readchar.readkey()
-        if str.isnumeric(key):
-            counted_numeric_key += 1
-        else:
-            counted_non_numeric_key += 1
-        print('Thank you for typing key ' +
-              Fore.GREEN + (key) + Style.RESET_ALL)
         if key == 'X':
             print('You broke the cicle Zé')
             break
+        else:
+            keys.append(key) #append adiciona elemento fim do array
+    print('The keys that you pressed are ' + str(keys))
+
+
+
+    #Analisar a lista e contar
+    counted_numeric_key = 0
+    counted_non_numeric_key = 0
+    lista_numeros = []
+    lista_outros = []
+
+    for i in keys:
+
+        if str.isnumeric(i):
+            counted_numeric_key += 1
+            lista_numeros.append(i)
+            #Exercicio 5 d)
+            lista_numeros= sorted(lista_numeros, reverse=False)
+
+        else:
+            counted_non_numeric_key += 1
+            lista_outros.append(i)
+            lista_outros= sorted(lista_outros, reverse=False)
+
+    print(Fore.LIGHTMAGENTA_EX + 'This are the numerics '
+                                 'that you pressed '
+          + str(lista_numeros) + Style.RESET_ALL )
+
+    print(Fore.YELLOW + 'This are the non numerics '
+                                 'that you pressed '
+          + str(lista_outros) + Style.RESET_ALL)
+
     print(Fore.BLUE+'You entered ' + str(counted_numeric_key) +
           ' numbers.'+Style.RESET_ALL)
     print(Fore.RED+'You entered ' + str(counted_non_numeric_key)
           + ' others.'+Style.RESET_ALL)
-def main():
 
-    # Ex4 b)
+#5 c)
+
+    dic_others = {}
+
+    for no, i in enumerate(keys):
+        if not str.isnumeric(i):
+            dic_others[no] = i
+
+    print(Fore.LIGHTBLACK_EX + 'dic_others ' + str(dic_others))
+
+
+def main():
     print('Aí estão os chars')
     readAllUpTo('X')
-
-    # Ex4 a)
-    #  print('Aí está o char')
-    #  key = readchar.readchar()
-    #  printAllCharsUpTo(key)
 
 
 if __name__ == '__main__':
