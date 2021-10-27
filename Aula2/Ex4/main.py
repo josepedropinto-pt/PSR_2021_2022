c508b50b3392d811ad98bd106cd0277b49424c42
josepedropinto-pt <josemcp@ua.pt>
2021-10-27 13:26:07 +0100

    New update the

--- Aula2/Ex4/main.py
+++ Aula2/Ex4/main.py
@@ -6,7 +6,7 @@ def printAllCharsUpTo(stop_char):
 
     for i in range(ord(' '), ord(stop_char)+1):
         print(chr(i))
-
+        print('teste')
 
 def main():
     print('Aí está o char')
@@ -18,3 +18,5 @@ if __name__ == '__main__':
     main()
 
 #comment to push from pycharm
+
+#send back to pych
