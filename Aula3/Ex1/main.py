25a15820a60add2cb03500ff69ea7686c00e9c3b
josepedropinto-pt <josemcp@ua.pt>
2021-10-27 23:29:28 +0100

    Ex1 completed

---
+++ Aula3/Ex1/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from colorama import Fore, Style,Back
+from time import time, ctime
+import math
+date = ctime()
+print("Today is "+Fore.LIGHTMAGENTA_EX + date + Style.RESET_ALL)
+
+init_time = time()
+
+for i in range(0, 500000):
+    math.sqrt(i)
+
+end_time = time()
+
+elapsed_time = end_time - init_time
+
+print('Total elapsed time of ' + Back.LIGHTCYAN_EX+str(elapsed_time)+Style.RESET_ALL +
+      " " + Fore.RED+ 'seconds'+Style.RESET_ALL)
