a269ad49be0e1b5a805aed104f04ab612e543c3d
josepedropinto-pt <josemcp@ua.pt>
2021-10-28 13:28:41 +0100

    Ex3 completed

---
+++ Aula3/Ex4/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+class Complex:
+
+    def __init__(self, r, i):
+        self.r = r  # store real part in class instance
+        self.i = i  # store imaginary part in class instance
+
+    def add(self, y):
+        # addapt code to use classes
+
+    def multiply(self, y):
+        # addapt code to use classes
+
+    def __str__(self):
+        # addapt code to use classes
+
+def main():
+    # declare two instances of class two complex numbers as tuples of size two
+    c1 = Complex(5, 3)  # use order when not naming
+    c2 = Complex(i=7, r=-2)  # if items are names order is not relevant
+
+    # Test add
+    print(c1)  # uses the __str__ method in the class
+    c1.add(c2)
+    print(c1)  # uses the __str__ method in the class
+
+    # test multiply
+    print(c2)  # uses the __str__ method in the class
+    c2.add(c1)
+    print(c2)  # uses the __str__ method in the class
