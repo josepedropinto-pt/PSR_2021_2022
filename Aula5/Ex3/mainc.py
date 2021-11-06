aaed20f8e4fe98abdd26c3f0830183be9afcf2ba
josepedropinto-pt <josemcp@ua.pt>
2021-11-06 19:47:41 +0000

    Open cv exercices until 3 c) done

--- Aula5/Ex3/mainc.py
+++ Aula5/Ex3/mainc.py
@@ -13,6 +13,20 @@ def onTrackbar(threshold, window_name, image_gray):
     cv.imshow(window_name, image_binary)
 
 
+def onMouse(event, x, y, flags, param):
+    if event == cv.EVENT_LBUTTONDOWN:
+        print("The coordinates are " +
+              str(x) + "," + str(y))
+
+    # if event == cv.EVENT_RBUTTONDOWN:
+    #     blue = image_gray[y, x, 0]
+    #     green = image_gray[y, x, 1]
+    #     red = image_gray[y, x, 2]
+    #
+    #     print('RGB : (' + str(red) + ' ' + str(green)
+    #           + ' ' + str(blue))
+
+
 def main():
     # Defining the argument parser to call the full path to image file
     parser = argparse.ArgumentParser()
@@ -32,6 +46,9 @@ def main():
 
     # Create trackbar ('name', 'window_name', 'default', 'max', func)
     cv.createTrackbar('binary', window_name, 0, 255, onTrackbar_call)
+
+    # Creat mouse click event on "Window" with function "onMouse"
+    cv.setMouseCallback(window_name, onMouse)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
