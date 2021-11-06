3db1e4e2c6b967bfff42399d536980df37a18d99
josepedropinto-pt <josemcp@ua.pt>
2021-11-06 01:32:21 +0000

    Open cv exercices until 2 f) done

--- Aula5/Ex2/mainf.py
+++ Aula5/Ex2/mainf.py
@@ -9,31 +9,30 @@ def main():
     # Import original image and convert to HSV
     original_image = cv.imread('atlas2000_e_atlasmv.png', 1)
     original_hsv = cv.cvtColor(original_image, cv.COLOR_BGR2HSV)
-    cv.imshow('original_RGB', original_image)
-    cv.imshow('original_HSV', original_hsv)
+    # cv.imshow('original_RGB', original_image)
+    # cv.imshow('original_HSV', original_hsv)
 
     # Mask the green box and print the mask
-    lower_thresh = np.array([60, 230, 80])
-    upper_thresh = np.array([70, 250, 105])
+    lower_thresh = np.array([50, 128, 50])
+    upper_thresh = np.array([100, 255, 100])
     mask_green_box = cv.inRange(original_hsv, lower_thresh, upper_thresh)
     cv.imshow('mask_green_box', mask_green_box)
 
     # Print the mask on the original image
-    green_box = cv.bitwise_and\
+    green_box = cv.bitwise_and \
         (original_image, original_image, mask=mask_green_box)
     cv.imshow('green box', green_box)
 
     # Paint the masked box with another color
-    red_box = cv.add(original_image, (0,0,256,0),
+    red_box = cv.add(original_image, (0, 0, 255, 0),
                      mask=mask_green_box, dst=original_image)
     # Function add uses merges 2 images or image and scalar with
     # a defined mask into a specific destination
     cv.imshow('red box image', red_box)
 
-
     cv.waitKey(0)
     cv.destroyAllWindows()
 
+
 if __name__ == "__main__":
     main()
-
